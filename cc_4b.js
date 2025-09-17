0d9a754ba4a89f4076e99c4c01a0a1db3a9b59a0
Francisco Quinones <fquinonescedeno@usf.edu>
2025-09-17 17:46:41 -0400

    Step 3

--- cc_4b.js
+++ cc_4b.js
@@ -1,11 +1,34 @@
 // Step 2 Define An Array
 
-let Products = [ 5 
-    {sku: "SKU-001", name: "Apples", category: "groceries", inventory: 30},
-    {sku: "SKU-002", name: "Grapes", category: "groceries", inventory: 20},
-    {sku: "SKU-003", name: "Phone Case", category: "electronics", inventory: 5},
-    {sku: "SKU-004", name: "Hat", category: "apparel", inventory: 6},
-    {sku: "SKU-005", name: "Bleach", category: "household", inventory: 5},
+let Products = [
+    {sku: "SKU-001", name: "Apples", category: "groceries", price: 3.99, inventory: 30},
+    {sku: "SKU-002", name: "Grapes", category: "groceries", price: 4.99, inventory: 20},
+    {sku: "SKU-003", name: "Phone Case", category: "electronics", price: 5.99, inventory: 5},
+    {sku: "SKU-004", name: "Hat", category: "apparel", price: 6.99, inventory: 6},
+    {sku: "SKU-005", name: "Bleach", category: "household", price: 7.99, inventory: 5},
 ];
 
-// Step 3
+// Step 3 Category Discounts
+
+for (let product of Products) {
+    let discount = 0
+
+    switch (product.category) {
+        case "electronics":
+            discount = 0.20;
+            break;
+        case "apparel":
+            discount = 0.15;
+            break;
+        case "groceries":
+        case "household":
+            discount = 0.10;
+            break
+        default:
+            discount = 0;
+    }
+let promoprice = product.price * (1-discount);
+product.promoprice = parseFloat(promoprice.toFixed(2));
+}
+
+console.log(Products);
