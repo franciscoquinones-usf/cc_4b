7269de85e1583b855ffdc1b4127bbab8d19802ef
Francisco Quinones <fquinonescedeno@usf.edu>
2025-09-17 17:20:51 -0400

    Step 2 complete

--- cc_4b.js
+++ cc_4b.js
@@ -1,2 +1,11 @@
-// Step 2 Define An Array Define an array of 5 products. Each product must include:
-// sku (string, unique), name (string), category (string), price (number), inventory (number)
+// Step 2 Define An Array
+
+let Products = [ 5 
+    {sku: "SKU-001", name: "Apples", category: "groceries", inventory: 30},
+    {sku: "SKU-002", name: "Grapes", category: "groceries", inventory: 20},
+    {sku: "SKU-003", name: "Phone Case", category: "electronics", inventory: 5},
+    {sku: "SKU-004", name: "Hat", category: "apparel", inventory: 6},
+    {sku: "SKU-005", name: "Bleach", category: "household", inventory: 5},
+];
+
+// Step 3
